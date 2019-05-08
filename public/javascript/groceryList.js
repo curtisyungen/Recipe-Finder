7574db8f527b580b267c6f0288376c7852d2103a
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-08 10:29:09 -0700

    Add files via upload

--- public/javascript/groceryList.js
+++ public/javascript/groceryList.js
@@ -292,7 +292,7 @@ $("#email").on("click", function() {
   var groceryList = $("#groceryList").children();
 
   for (var recipe in groceryList) {
-    console.log(groceryList[recipe].dataset);
+    console.log(groceryList[recipe]);
   }
 });
 
