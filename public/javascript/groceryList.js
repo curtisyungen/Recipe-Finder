803af73ee57aaee26cb3396c67d8889689363cb2
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-08 11:49:05 -0700

    Add files via upload

--- public/javascript/groceryList.js
+++ public/javascript/groceryList.js
@@ -255,6 +255,8 @@ function crossOffList() {
 $(document).on("click", ".deleteBtn", function (event) {
   event.preventDefault();
 
+  console.log($(this).parent());
+
   removeFromGroceryList($(this).parent().dataset);
 
   $(this).parent().remove();
